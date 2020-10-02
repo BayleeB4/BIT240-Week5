a036b085db6edcdf07fa7ca6188b6bc28c4042f9
Baylee <70728997+BayleeB4@users.noreply.github.com>
2020-10-01 20:18:04 -0500

    Update monsters.js

--- Week 5/monsters.js
+++ Week 5/monsters.js
@@ -2,16 +2,19 @@ var listOfMonsters = [
 	{
 		name: "Goblin",
 		hp: 8,
-		damage: 2
+		damage: 2,
+		gold: 2
 	},
 	{
 		name: "Orc",
 		hp: 35,
-		damage: 13
+		damage: 13,
+		gold: 6
 	},
 	{
 		name: "Troll",
 		hp: 72,
-		damage: 25
+		damage: 25,
+		gold: 10
 	}
 ]
